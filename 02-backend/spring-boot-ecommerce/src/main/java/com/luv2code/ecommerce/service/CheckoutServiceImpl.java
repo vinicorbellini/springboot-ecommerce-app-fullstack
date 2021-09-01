057e76905668d68ddcd232108fe55e1af45447b1
Vinicius Corbellini <corbelliniv@gmail.com>
2021-09-01 16:16:23 +0200

    add files

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -0,0 +1,71 @@
+package com.luv2code.ecommerce.service;
+
+import com.luv2code.ecommerce.dao.CustomerRepository;
+import com.luv2code.ecommerce.dto.Purchase;
+import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.entity.Customer;
+import com.luv2code.ecommerce.entity.Order;
+import com.luv2code.ecommerce.entity.OrderItem;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService {
+
+    private CustomerRepository customerRepository;
+
+    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    @Transactional
+    public PurchaseResponse placeOrder(Purchase purchase) {
+
+        // retrieve the order info from dto
+        Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(order::add);
+
+        // populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        // populate customer with order
+        Customer customer = purchase.getCustomer();
+
+        // check if it's existing customer
+        String theEmail = customer.getEmail();
+
+        Customer customerFromDB = customerRepository.findByEmail(theEmail);
+
+        if (customerFromDB != null) {
+            // found, so assign accordingly
+            customer = customerFromDB;
+        }
+        
+        customer.add(order);
+
+        // save to the database
+        customerRepository.save(customer);
+
+        // return a response
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // generate random UUID number (UUID version-4)
+        // for details see: https://en.wikipedia.org/wiki/Universally_unique_identifier
+        return UUID.randomUUID().toString();
+    }
+}
